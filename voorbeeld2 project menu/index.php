94b60565a3db3b20c49643f82c29a00e4b1bf4eb
EmirhanK010 <9021587@student.zadkine.nl>
2023-12-14 09:54:57 +0100

    dj

--- voorbeeld2 project menu/index.php
+++ voorbeeld2 project menu/index.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Website pages</title>
+    <title>voorbeeld2 menu project</title>
 </head>
 <body>
-    
+    <h1>voorbeeld2 menu project</h1>
 
-
-
+    <?php
+        include "menu.php";
+    ?>
     
 </body>
 </html>
